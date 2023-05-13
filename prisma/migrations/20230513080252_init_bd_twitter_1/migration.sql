24b6b885871511d472d0e0536e073d0481b3a2a3
mdsiamss10 <mdsiamss10@gmail.com>
2023-05-13 14:20:07 +0600

    INitial COmmit

---
+++ prisma/migrations/20230513080252_init_bd_twitter_1/migration.sql
@@ -0,0 +1,108 @@
+-- CreateTable
+CREATE TABLE "Tweet" (
+    "id" STRING NOT NULL,
+    "userId" STRING NOT NULL,
+    "content" STRING NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Tweet_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Like" (
+    "userId" STRING NOT NULL,
+    "tweetId" STRING NOT NULL,
+
+    CONSTRAINT "Like_pkey" PRIMARY KEY ("userId","tweetId")
+);
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" STRING NOT NULL,
+    "userId" STRING NOT NULL,
+    "type" STRING NOT NULL,
+    "provider" STRING NOT NULL,
+    "providerAccountId" STRING NOT NULL,
+    "refresh_token" STRING,
+    "access_token" STRING,
+    "expires_at" INT4,
+    "token_type" STRING,
+    "scope" STRING,
+    "id_token" STRING,
+    "session_state" STRING,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" STRING NOT NULL,
+    "sessionToken" STRING NOT NULL,
+    "userId" STRING NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" STRING NOT NULL,
+    "name" STRING,
+    "email" STRING,
+    "emailVerified" TIMESTAMP(3),
+    "image" STRING,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "identifier" STRING NOT NULL,
+    "token" STRING NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_Followers" (
+    "A" STRING NOT NULL,
+    "B" STRING NOT NULL
+);
+
+-- CreateIndex
+CREATE INDEX "Tweet_userId_idx" ON "Tweet"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tweet_createdAt_id_key" ON "Tweet"("createdAt", "id");
+
+-- CreateIndex
+CREATE INDEX "Like_userId_idx" ON "Like"("userId");
+
+-- CreateIndex
+CREATE INDEX "Like_tweetId_idx" ON "Like"("tweetId");
+
+-- CreateIndex
+CREATE INDEX "Account_userId_idx" ON "Account"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
+
+-- CreateIndex
+CREATE INDEX "Session_userId_idx" ON "Session"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_Followers_AB_unique" ON "_Followers"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_Followers_B_index" ON "_Followers"("B");
